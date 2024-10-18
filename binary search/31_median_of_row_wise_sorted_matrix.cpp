09f7d65e35b40ea81f73f54d2b907160e5145b18
sangam verma <sangamverma955911@gmail.com>
2024-10-18 11:50:05 +0530

    18/10/24

--- binary search/31_median_of_row_wise_sorted_matrix.cpp
+++ binary search/31_median_of_row_wise_sorted_matrix.cpp
@@ -1,7 +1,47 @@
-
-#include<bits/stdc++.h>
+// https://www.geeksforgeeks.org/problems/median-in-a-row-wise-sorted-matrix1527/1
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+class Solution
+{
+public:
+    int lesserthan(int mid, vector<vector<int>> &matrix, int R, int C) // this function finds no of lesser elements than mid
+    {
+        int t = 0;
+        for (int i = 0; i < R; i++)
+        {
+            int ind = upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+            t += ind;
+        }
+        return t;
+    }
+    int median(vector<vector<int>> &matrix, int R, int C)
+    {
+        int low = INT_MAX, high = INT_MIN;
+        // median lies between minele and maxiele
+        for (int i = 0; i < R; i++)
+        {
+            low = min(low, matrix[i][0]);
+            high = max(high, matrix[i][C - 1]);
+        }
+        // idea is that median is always greater than no of elements req((R*C)/2)
+        int req = (R * C) / 2;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (lesserthan(mid, matrix, R, C) <= req)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+};
+int main()
+{
 
-return 0;
+    return 0;
 }
